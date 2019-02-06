44092c3140729eedef6b93c17895fb931e7159fc
Hugh Natt <hughnatt@protonmail.com>
2019-02-06 17:22:19 +0100

    Implemented the performance test

---
+++ src/tests/PerfsComparison.java
@@ -0,0 +1,47 @@
+package tests;
+
+/**
+ * Test class for comparing the different server implementations
+ * This will create multiple clients and display
+ * NB: The server must be running before starting the test1
+ */
+public class PerfsComparison {
+
+	private final static int NCLIENTS = 10;
+	private final static String SERVER = "127.0.0.1";
+	private final static int PORT = 4242;
+	private final static String FILE = "chameau.jpg";
+	
+	public static void main(String args[]) {
+		
+		System.out.println("Launch the server you want to test");
+		
+		MultiClient clients[] = new MultiClient[NCLIENTS];
+		
+		for (int i = 0; i < NCLIENTS; i++) {
+			clients[i] = new MultiClient(SERVER,PORT,FILE);
+		}
+		
+		long start,end;
+		
+		start = System.currentTimeMillis();
+		
+		for (int i = 0; i < NCLIENTS; i++) {
+			clients[i].start();
+		}
+		
+		for (int i = 0; i < NCLIENTS; i++) {
+			try {
+				clients[i].join();
+			} catch (InterruptedException e) {
+				System.out.println("An interruption occured, displayed time might not be accurate");
+			}
+		}
+		
+		end = System.currentTimeMillis();
+		
+		long time = end - start;
+		
+		System.out.println("All files retrieved in " + time + " ms");
+	}
+}
